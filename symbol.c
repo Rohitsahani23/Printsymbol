f61e84d97b5d5661c07b3ca663402df6e4c2331f
Rohitsahani23 <rohitsahani612@gmail.com>
2024-09-19 08:55:31 +0530

    Add files via upload

---
+++ symbol.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+ clrscr();
+ printf("Enter a number of line\n");
+ scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+for(j=i;j<n;j++) 
+{
+ printf(" ");
+}
+for(j=1;j<=i;j++)
+{
+ printf("#");
+}
+printf("\n");
+}
+
+
+for(i=1;i<=n;i++)
+{
+for(j=n;j<i;j++) 
+{
+ printf(" ");
+}
+for(j=1;j<=i;j++)
+{
+ printf("#");
+}
+printf("\n");
+}
+
+ getch();
+}
